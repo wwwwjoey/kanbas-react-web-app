5d8e2ee4ca57990ca53c6f864f67102697cec607
Joe Yu <wwwwjoey@gmail.com>
2024-05-21 00:53:08 -0400

    6th commit

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -4,6 +4,8 @@ import KanbasNavigation from "./Navigation";
 import { Routes, Route, Navigate } from "react-router";
 import Courses from "./Courses";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Labs from "../Labs";
+import Lab1 from "../Labs/Lab1";
 
 
 export default function Kanbas() {
@@ -16,6 +18,7 @@ export default function Kanbas() {
               <Route path="/" element={<Navigate to="Dashboard" />} />
               <Route path="Dashboard" element={<Dashboard />} />
               <Route path="Courses/:id/*" element={<Courses />} />
+              <Route path="/Labs/*" element={<Labs />} />
             </Routes>
             </div>
             </div>
--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -32,7 +32,7 @@ export default function TOC() {
         </a>
       </li>
       <li className="nav-item">
-        <a id="wd-k" href="https://github.com/wwwwjoey" className="nav-link">
+        <a id="wd-github" href="https://github.com/wwwwjoey" className="nav-link">
           My GitHub
         </a>
       </li>
