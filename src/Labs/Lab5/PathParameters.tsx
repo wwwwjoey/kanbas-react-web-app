72c8fc7f73b0e80e98968bad5581cc05d9a286fa
wwwwjoey <132865060+wwwwjoey@users.noreply.github.com>
2024-06-12 03:51:10 -0400

    Update PathParameters.tsx

--- src/Labs/Lab5/PathParameters.tsx
+++ src/Labs/Lab5/PathParameters.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-const REMOTE_SERVER = "http://localhost:4000"
+const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
 export default function PathParameters() {
   const [a, setA] = useState("34");
   const [b, setB] = useState("23");
