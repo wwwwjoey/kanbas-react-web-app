215c17b9a76e9c8f8c99b91b6c644af151b0aaad
wwwwjoey <132865060+wwwwjoey@users.noreply.github.com>
2024-06-12 03:22:42 -0400

    Update index.tsx

--- src/Labs/Lab5/index.tsx
+++ src/Labs/Lab5/index.tsx
@@ -7,7 +7,7 @@ import WorkingWithArraysAsynchronously from "./WorkingWithArraysAsynchronously";
 import WorkingWithObjects from "./WorkingWithObjects";
 import WorkingWithObjectsAsynchronously from "./WorkingWithObjectsAsynchronously";
 
-const REMOTE_SERVER = "http://localhost:4000"
+const REMOTE_SERVER = "http://localhost:10000"
 export default function Lab5() {
   return (
     <div id="wd-lab5">
