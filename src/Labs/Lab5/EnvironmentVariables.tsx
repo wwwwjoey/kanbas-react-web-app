912927e0ed190e0eee48d1be70779a13a2696096
wwwwjoey <132865060+wwwwjoey@users.noreply.github.com>
2024-06-12 03:50:50 -0400

    Update EnvironmentVariables.tsx

--- src/Labs/Lab5/EnvironmentVariables.tsx
+++ src/Labs/Lab5/EnvironmentVariables.tsx
@@ -1,4 +1,4 @@
-const REMOTE_SERVER = "http://localhost:4000"
+const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
 export default function EnvironmentVariables() {
   return (
     <div id="wd-environment-variables">
