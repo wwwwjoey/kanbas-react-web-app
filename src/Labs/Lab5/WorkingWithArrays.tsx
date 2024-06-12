89b020767f81777c84dabf2e81ccd2965dc97905
wwwwjoey <132865060+wwwwjoey@users.noreply.github.com>
2024-06-12 03:51:27 -0400

    Update WorkingWithArrays.tsx

--- src/Labs/Lab5/WorkingWithArrays.tsx
+++ src/Labs/Lab5/WorkingWithArrays.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-const REMOTE_SERVER = 'http://localhost:4000';
+const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
 
 export default function WorkingWithArrays() {
     
