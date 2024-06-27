366e4857e0a0f81512ba508cc468708d618e6d33
wwwwjoey <132865060+wwwwjoey@users.noreply.github.com>
2024-06-27 14:22:09 -0400

    Create AccountNavigation.tsx

---
+++ src/Kanbas/Account/Navigation/AccountNavigation.tsx
@@ -0,0 +1,24 @@
+import { useSelector } from "react-redux";
+import { Link, useLocation } from "react-router-dom";
+
+export default function AccountNavigation() {
+  const { currentUser } = useSelector((state: any) => state.accountReducer);
+
+  const links = currentUser ? ["Profile"] : ["Signin", "Signup"];
+  const { pathname } = useLocation();
+
+  return (
+    <div id="wd-courses-navigation" className="list-group fs-5 rounded-0">
+      {links.map((link) => (
+        <Link
+          key={link} 
+          to={`/Kanbas/Account/${link}`}
+          className={`border border-0 list-group-item 
+          ${pathname.includes(link) ? "active text-black" : "text-danger"}`}
+        >
+          {link}
+        </Link>
+      ))}
+    </div>
+  );
+}
