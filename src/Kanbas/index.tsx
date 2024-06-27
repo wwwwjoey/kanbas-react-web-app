3e601faa8b1ebe3f008de72a4cc7d6b9aecdbcd0
wwwwjoey <132865060+wwwwjoey@users.noreply.github.com>
2024-06-27 14:57:45 -0400

    Add files via upload

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -1,28 +1,39 @@
-import "./styles.css";
+import { Navigate, Route, Routes } from "react-router";
+import Courses from "./Courses";
 import Dashboard from "./Dashboard";
 import KanbasNavigation from "./Navigation";
-import { Routes, Route, Navigate } from "react-router";
-import Courses from "./Courses";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Labs from "../Labs";
-import Lab1 from "../Labs/Lab1";
-import { useEffect, useState } from "react";
+import "./styles.css";
+import { useState } from "react";
 import store from "./store";
 import { Provider } from "react-redux";
 import * as client from "./Courses/client";
-
+import { useEffect } from "react";
+import Account from "./Account";
+import Session from "./Account/Session";
+import ProtectedRoute from "./Account/ProtectedRoute";
 
 export default function Kanbas() {
   const [courses, setCourses] = useState<any[]>([]);
+
+  const fetchCourses = async () => {
+    const courses = await client.fetchAllCourses();
+    setCourses(courses);
+  };
+
   const [course, setCourse] = useState<any>({
-    _id: "1234", name: "New Course", number: "New Number",
-    startDate: "2023-09-10", endDate: "2023-12-15", description: "New Description",
+    _id: "0",
+    name: "New Course",
+    number: "New Number",
+    startDate: "2023-09-10",
+    endDate: "2023-12-15",
+    image: "/images/reactjs.jpg",
+    description: "New Description",
   });
   const addNewCourse = async () => {
     const newCourse = await client.createCourse(course);
-    setCourses([ ...courses, newCourse ]);
+    setCourses([newCourse, ...courses]);
   };
-  const deleteCourse = async (courseId: any) => {
+  const deleteCourse = async (courseId: string) => {
     await client.deleteCourse(courseId);
     setCourses(courses.filter((course) => course._id !== courseId));
   };
@@ -38,43 +49,53 @@ export default function Kanbas() {
       })
     );
   };
-  const fetchCourses = async () => {
-    const courses = await client.fetchAllCourses();
-    setCourses(courses);
-  };
+
   useEffect(() => {
     fetchCourses();
   }, []);
 
-
   return (
     <Provider store={store}>
-    <div id="wd-kanbas" className="h-100">
-      <div className="d-flex h-100">
-        <div className="d-none d-md-block bg-black">
-          <KanbasNavigation />
+      <Session>
+        <div id="wd-kanbas" className="h-100">
+          <div className="d-flex h-100">
+            <div className="bg-black d-none d-md-block h-100">
+              <KanbasNavigation />
+            </div>
+            <div className="flex-fill p-4">
+              <Routes>
+                <Route path="/" element={<Navigate to="Dashboard" />} />
+                <Route path="Account/*" element={<Account />} />
+                <Route
+                  path="Dashboard"
+                  element={
+                    <ProtectedRoute>
+                      <Dashboard
+                        courses={courses}
+                        course={course}
+                        setCourse={setCourse}
+                        addNewCourse={addNewCourse}
+                        deleteCourse={deleteCourse}
+                        updateCourse={updateCourse}
+                      />
+                    </ProtectedRoute>
+                  }
+                />
+                <Route
+                  path="Courses/:cid/*"
+                  element={
+                    <ProtectedRoute>
+                      <Courses courses={courses} />
+                    </ProtectedRoute>
+                  }
+                />
+                <Route path="Calendar" element={<h1>Calendar</h1>} />
+                <Route path="Inbox" element={<h1>Inbox</h1>} />
+              </Routes>
+            </div>
+          </div>
         </div>
-        <div className="flex-fill p-4">
-          <Routes>
-            <Route path="/" element={<Navigate to="Dashboard" />} />
-            <Route path="Account" element={<h1>Account</h1>} />
-            <Route path="Dashboard" element={            <Dashboard
-              courses={courses}
-              course={course}
-              setCourse={setCourse}
-              addNewCourse={addNewCourse}
-              deleteCourse={deleteCourse}
-              updateCourse={updateCourse}/>} />
-            <Route path="Courses/:cid/*" element={<Courses courses={courses} />} />
-            <Route path="Calendar" element={<h1>Calendar</h1>} />
-            <Route path="Inbox" element={<h1>Inbox</h1>} />
-          </Routes>
-        </div>
-      </div>
-    </div>
+      </Session>
     </Provider>
-
-);}
-
-  
-  
+  );
+}
--- src/Kanbas/store.ts
+++ src/Kanbas/store.ts
@@ -1,11 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
 import modulesReducer from './Courses/Modules/reducer';
 import assignmentsReducer from './Courses/Assignments/reducer';
+import accountReducer from "./Account/reducer";
+import quizzesReducer from "./Courses/Quizzes/reducer";
+
 
 const store = configureStore({
   reducer: {
-    modules: modulesReducer,
+    modulesReducer,
     assignments: assignmentsReducer,
+    accountReducer,
+    quizzesReducer,
   },
 });
 
