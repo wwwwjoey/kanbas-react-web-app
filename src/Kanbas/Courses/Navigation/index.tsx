e18e7e38b4fe00c8bd166ce4664e4eb377226a1b
wwwwjoey <132865060+wwwwjoey@users.noreply.github.com>
2024-06-27 14:39:38 -0400

    Add files via upload

--- src/Kanbas/Courses/Navigation/index.tsx
+++ src/Kanbas/Courses/Navigation/index.tsx
@@ -1,32 +1,31 @@
-import { useLocation, useParams } from "react-router";
+
+
 import "./index.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Link } from "react-router-dom";
+import { Link, useLocation, useParams } from 'react-router-dom';
 
-const links = ["Home", "Modules", "Piazza", "Zoom", "Assignments", "Quizzes", "Grades"];
+export default function CoursesNavigation() {
+    const { cid } = useParams();
+    const { pathname } = useLocation();
+    
+    const links = ["Home", "Modules", "Piazza", "Zoom", "Assignments", "Quizzes", "Grades", "People"];
+    
+    return (
+        <div id="wd-courses-navigation" className="list-group fs-5 rounded-0">
+            {links.map(link => {
+                const linkPath = `/Kanbas/Courses/${cid}/${link}`;
+                const isActive = pathname === linkPath;
 
-const CoursesNavigation: React.FC = () => {
-  const { cid } = useParams<{ cid: string }>();
-  const location = useLocation();
-
-  return (
-    <div id="wd-courses-navigation" className="list-group fs-5 rounded-0">
-      {links.map((link) => {
-        const path = `/Kanbas/Courses/${cid}/${link}`;
-        const isActive = location.pathname === path;
-        return (
-          <Link
-            key={link}
-            to={path}
-            className={`list-group-item border border-0 ${isActive ? "active" : "text-danger"}`}
-          >
-            {link}
-          </Link>
-        );
-      })}
-    </div>
-  );
-};
-
-export default CoursesNavigation;
+                return (
+                    <Link 
+                        key={link}
+                        to={linkPath}
+                        className={`list-group-item ${isActive ? 'active' : 'text-danger'} border-0`}
+                    >
+                        {link}
+                    </Link>
+                );
+            })}
+        </div>
+    );
+}
 
